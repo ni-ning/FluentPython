f4323803458d876301d69ffac522459e42a948f6
nizhiyuan2007@126.com <nizhiyuan2007@126.com>
2020-05-02 00:00:15 +0800

    feat: add 9

---
+++ 第 9 章 符合Python风格的对象/vector2d_v3.py
@@ -0,0 +1,66 @@
+# coding: utf-8
+
+from array import array
+import math
+
+
+class Vector2d:
+    type_code = 'd'
+
+    def __init__(self, x, y):
+        self.__x = x
+        self.__y = y
+
+    @property
+    def x(self):
+        return self.__x
+
+    @property
+    def y(self):
+        return self.__y
+
+    def __iter__(self):
+        return (i for i in (self.x, self.y))
+
+    def __repr__(self):
+        class_name = type(self).__name__
+        return '{}({!r}, {!r})'.format(class_name, *self)
+
+    def __str__(self):
+        return str(tuple(self))
+
+    def __bytes__(self):
+        return bytes(ord(self.type_code)) + \
+               bytes(array(self.type_code, self))
+
+    def __eq__(self, other):
+        return tuple(self) == tuple(other)
+
+    def __hash__(self):
+        return hash(self.x) ^ hash(self.y)
+
+    def __abs__(self):
+        return math.hypot(self.x, self.y)
+
+    def __bool__(self):
+        return bool(abs(self))
+
+    def angle(self):
+        return math.atan2(self.y, self.x)
+
+    def __format__(self, format_spec=''):
+        if format_spec.endswith('p'):
+            format_spec = format_spec[:-1]
+            coords = (abs(self), self.angle())
+            outer_fmt = '<{}, {}>'
+        else:
+            coords = self
+            outer_fmt = '({}, {})'
+        components = (format(c, format_spec) for c in coords)
+        return outer_fmt.format(*components)
+
+    @classmethod
+    def from_bytes(cls, octets):
+        type_code = chr(octets[0])
+        memv = memoryview(octets[1:0]).cast(type_code)
+        return cls(*memv)
